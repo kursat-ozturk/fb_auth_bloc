1ef3129c00c682f6170abcd40bdb06caf17f5de4
Kürşat Öztürk <kursatsoftware@gmail.com>
2024-01-23 14:17:40 +0300

    created ProfilePage widget

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,8 @@
+import 'package:fb_auth_bloc/blocs/auth/auth_bloc.dart';
+import 'package:fb_auth_bloc/blocs/profile/profile_cubit.dart';
+import 'package:fb_auth_bloc/utils/error_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -9,10 +13,109 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   @override
+  void initState() {
+    super.initState();
+    _getProfile();
+  }
+
+  void _getProfile() {
+    final String uid = context.read<AuthBloc>().state.user!.uid;
+    print('uid: $uid');
+    context.read<ProfileCubit>().getProfile(uid: 'uid');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Profile'),
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: BlocConsumer<ProfileCubit, ProfileState>(
+        listener: (context, state) {
+          if (state.profileStatus == ProfileStatus.error) {
+            errorDialog(context, state.error);
+          }
+        },
+        builder: (context, state) {
+          if (state.profileStatus == ProfileStatus.initial) {
+            return Container();
+          } else if (state.profileStatus == ProfileStatus.loading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state.profileStatus == ProfileStatus.error) {
+            return Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/error.png',
+                    width: 75,
+                    height: 75,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(width: 20),
+                  Text(
+                    'Ooops!\nTry again',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+          return Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FadeInImage.assetNetwork(
+                  placeholder: 'assets/images/loading.gif',
+                  image: state.user.profileImage,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '-id: ${state.user.id}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        '-id: ${state.user.name}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        '-id: ${state.user.email}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        '-id: ${state.user.point}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        '-id: ${state.user.rank}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
