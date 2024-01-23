975174887c28e57dc022b7ad3697bab6cce00ceb
Kürşat Öztürk <kursatsoftware@gmail.com>
2024-01-23 12:14:12 +0300

    created ProfileRepository and ProfileCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fb_auth_bloc/blocs/auth/auth_bloc.dart';
+import 'package:fb_auth_bloc/blocs/profile/profile_cubit.dart';
 import 'package:fb_auth_bloc/blocs/signin/signin_cubit.dart';
 import 'package:fb_auth_bloc/blocs/signup/signup_cubit.dart';
 import 'package:fb_auth_bloc/pages/home_page.dart';
@@ -7,6 +8,7 @@ import 'package:fb_auth_bloc/pages/signin_page.dart';
 import 'package:fb_auth_bloc/pages/signup_page.dart';
 import 'package:fb_auth_bloc/pages/splash_page.dart';
 import 'package:fb_auth_bloc/repositories/auth_repository.dart';
+import 'package:fb_auth_bloc/repositories/profile_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -34,6 +36,11 @@ class MyApp extends StatelessWidget {
             firebaseAuth: FirebaseAuth.instance,
           ),
         ),
+        RepositoryProvider<ProfileRepository>(
+          create: (context) => ProfileRepository(
+            firebaseFirestore: FirebaseFirestore.instance,
+          ),
+        ),
       ],
       child: MultiBlocProvider(
         providers: [
@@ -50,6 +57,11 @@ class MyApp extends StatelessWidget {
           BlocProvider<SignupCubit>(
             create: (context) => SignupCubit(
               authRepository: context.read<AuthRepository>(),
+            ),
+          ),
+          BlocProvider<ProfileCubit>(
+            create: (context) => ProfileCubit(
+              profileRepository: context.read<ProfileRepository>(),
             ),
           ),
         ],
